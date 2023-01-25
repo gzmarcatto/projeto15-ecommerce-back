b2bc02e9eec2dbd571b151c7f5e2b60269bf87ca
Telmo Junior <telmobsj@gmail.com>
2023-01-25 12:26:42 -0300

    arquitetura base

---
+++ src/database/db.js
@@ -0,0 +1,16 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI)
+
+try{
+    await mongoClient.connect();
+    console.log("Connected to database");
+} catch(err){
+    console.log(err);
+}
+
+const db = mongoClient.db();
+
+export const productsColletion = db.collection("products");
