3070e2dde64c063277e22c41221401b8a337cd87
Telmo Junior <telmobsj@gmail.com>
2023-01-27 12:27:35 -0300

    corrigindo server

--- src/server.js
+++ src/server.js
@@ -3,10 +3,15 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import authRoutes from './routes/auth.routes.js';
+import productsRoutes from './routes/products.routes.js';
+
 const server = express();
 
 server.use(express.json());
 server.use(cors());
+
+server.use([authRoutes, productsRoutes])
 
 server.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
